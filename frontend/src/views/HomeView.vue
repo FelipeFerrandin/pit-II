88b36f821843969cea3a3b8db897470c55802d4c
Felipe <felipeferrandin@ojo.com.br>
2023-10-28 18:22:02 -0300

    init

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <v-container>
+
+      <v-row>
+        <v-col v-for="n in 24" :key="n" cols="12" xs="12" sm="12" md="6" lg="4">
+          <v-card height="200"></v-card>
+        </v-col>
+      </v-row>
+
+    </v-container>
   </main>
 </template>
