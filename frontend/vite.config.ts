cc96a94be0eea61884b0e9747cf28229d00bd64d
Felipe <felipeferrandin@ojo.com.br>
2023-11-18 12:13:12 -0300

    alteracoes de teste para prod

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,9 +8,6 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  server: {
-    port: 80
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
