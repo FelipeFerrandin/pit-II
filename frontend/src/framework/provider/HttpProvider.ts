51fb4fae1f6f7c59d703b14060447578894b4c24
Felipe <felipeferrandin@ojo.com.br>
2023-11-18 12:12:16 -0300

    alteracoes de teste para prod

--- frontend/src/framework/provider/HttpProvider.ts
+++ frontend/src/framework/provider/HttpProvider.ts
@@ -10,7 +10,7 @@ export default class HttpProvider {
 
   private static createAxiosInstanceBase() {
     const instance = axios.create({
-      baseURL: "http://localhost:3000",
+      baseURL: "http://localhost:8080",
       headers: {
         "Content-Type": "application/json"
       }
